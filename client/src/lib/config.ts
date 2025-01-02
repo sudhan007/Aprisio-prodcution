b232e003588322545a454114e6052ea5466ad4e9
Stains Rubus <stainsrubus@gmail.com>
2025-01-02 10:44:47 +0530

    baseurl changed in config

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,2 +1,6 @@
 // export const baseUrl = "http://localhost:4000/api/user";
-export const baseUrl = "https://aprisio-prodcution.onrender.com/api/user";
+
+//export const baseUrl = "https://aprisio-prodcution.onrender.com/api/user";
+
+
+export const baseUrl = "https://aprisio-prodcution-v9o7.onrender.com/api/user";
