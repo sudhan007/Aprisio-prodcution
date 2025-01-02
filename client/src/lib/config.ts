12c3611082a2b6599f166602b7cacf22cbcf430b
Stains Rubus <stainsrubus@gmail.com>
2025-01-02 10:39:08 +0530

    minor changes done

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://localhost:4000/api/user";
-export const baseUrl = "https://aprisio-prodcution.onrender.com:4000/api/user";
+export const baseUrl = "https://aprisio-prodcution.onrender.com/api/user";
